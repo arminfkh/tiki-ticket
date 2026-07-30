05b0c2517199aa9decd4f6250104592682eb386a
arminfakhar <arminfakhar1384@gmail.com>
2026-07-30 19:36:18 +0330

    chore(api): configure API module routes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import hello
+# core/urls.py
+
+from django.urls import path, include
 
 urlpatterns = [
-    path("hello/", hello),
+    path('auth/', include('core.api.auth.urls')),
+    path('bookings/', include('core.api.bookings.urls')),
+    path('locations/', include('core.api.locations.urls')),
+    path('payments/', include('core.api.payments.urls')),
+    path('profile/', include('core.api.profile.urls')),
+    path('reports/', include('core.api.reports.urls')),
+    path('reservations/', include('core.api.reservations.urls')),
+    path('support/', include('core.api.support.urls')),
+    path('tickets/', include('core.api.tickets.urls')),
 ]
